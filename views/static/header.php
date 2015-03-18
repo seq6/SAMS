b4e15e61b6db2d0c7a5500f86e6b35d28bc66855
zhangSTC <807718857@qq.com>
2015-03-18 18:12:54 +0800

    v0.9

--- views/static/header.php
+++ views/static/header.php
@@ -1,10 +1,23 @@
-<header class="am-topbar">
-  <h1>
+<!--[if lte IE 9]>
+<p class="browsehappy">你正在使用<strong>过时</strong>的浏览器，请升级浏览器以获得更好的体验！</p>
+<![endif]-->
+
+<header class="am-topbar admin-header">
+  <div class="am-topbar-brand">
     <a href="#">安全风险评估管理系统</a>
     <span class="am-badge am-badge-danger">v0.7</span>
-  </h1>
+  </div>
+
+  <button class="am-topbar-btn am-topbar-toggle am-btn am-btn-sm am-btn-success am-show-sm-only" data-am-collapse="{target: '#topbar-collapse'}">
+    <span class="am-sr-only"> 导航切换</span>
+    <span class="am-icon-bars"></span>
+  </button>
+
+  <div class="am-collapse am-topbar-collapse" id="topbar-collapse">
+
   <ul class="am-nav am-nav-pills am-topbar-nav am-topbar-right admin-header-list">
     <li><a href="els_help"><span class="am-icon-puzzle-piece"></span> 帮助</a></li>
+    <li><a href="els_help"><span class="am-icon-user-plus"></span> 注册</a></li>
     <li><a href="els_login"><span class="am-icon-sign-out"></span> 注销</a></li> 
   </ul>
 </header>
--- views/static/sidebar.php
+++ views/static/sidebar.php
@@ -1,24 +1,40 @@
 <div class="admin-sidebar am-offcanvas" id="admin-offcanvas">
   <div class="am-offcanvas-bar admin-offcanvas-bar">
-    <!-- <ul class="am-nav"> -->
     <ul class="am-list admin-sidebar-list">
-      <li class="am-nav-header">前期准备</li>
-      <li><a href="pre_intro">项目简介</a></li>
-      <li><a href="pre_info">项目信息</a></li>
-      <li><a href="pre_menber">项目人员</a></li>
-      <li><a href="pre_parts">评估双方</a></li>
-      <li><a href="pre_other">其他任务</a></li>
-      <li><a href="pre_start">项目启动</a></li>
-      <li class="am-nav-header">系统调查</li>
-      <li><a href="sur_hard">硬件资源</a></li>
-      <li><a href="sur_soft">软件资源</a></li>
-      <li><a href="sur_person">人员资源</a></li>
-      <li><a href="sur_envir">物理环境</a></li>
-      <li><a href="sur_busin">业务系统</a></li>
-      <li><a href="sur_net">网络系统</a></li>
-      <li class="am-nav-header">脆弱性调查</li>
-      <li class="am-nav-header">威胁分析</li>
-      <li class="am-nav-header">风险分析</li>
+      <li><a href="admin-index.html"><span class="am-icon-home"></span>
+        <?php echo $data['project']['name']; ?></a>
+      </li>
+      <li class="admin-parent">
+        <a class="am-cf" data-am-collapse="{target: '#collapse-nav'}"> 前期准备</a>
+        <ul class="am-list am-collapse admin-sidebar-sub am-in" id="collapse-nav">
+          <li><a href="pre_intro">项目简介</a></li>
+          <li><a href="pre_info">项目信息</a></li>
+          <li><a href="pre_menber">项目人员</a></li>
+          <li><a href="pre_parts">评估双方</a></li>
+          <li><a href="pre_other">其他任务</a></li>
+          <li><a href="pre_start">项目启动</a></li>
+        </ul>
+      </li>
+      <li class="admin-parent">
+        <a class="am-cf" data-am-collapse="{target: '#collapse-nav'}"> 系统调查</a>
+        <ul class="am-list am-collapse admin-sidebar-sub am-in" id="collapse-nav">
+          <li><a href="sur_hard">硬件资源</a></li>
+          <li><a href="sur_soft">软件资源</a></li>
+          <li><a href="sur_person">人员资源</a></li>
+          <li><a href="sur_envir">物理环境</a></li>
+          <li><a href="sur_busin">业务系统</a></li>
+          <li><a href="sur_net">网络系统</a></li>
+        </ul>
+      </li>
+      <li class="admin-parent">
+        <a class="am-cf" data-am-collapse="{target: '#collapse-nav'}"> 脆弱性调查</a>
+      </li>
+      <li class="admin-parent">
+        <a class="am-cf" data-am-collapse="{target: '#collapse-nav'}"> 威胁分析</a>
+      </li>
+      <li class="admin-parent">
+        <a class="am-cf" data-am-collapse="{target: '#collapse-nav'}"> 风险分析</a>
+      </li>
     </ul>
 
     <div class="am-panel am-panel-default admin-sidebar-panel">
